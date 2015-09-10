c2d78599107b8d5ae7fde07f5d75671fb2443846
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-10 13:39:49 +0200

    Remove old controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,16 +11,3 @@ cvAppControllers.controller('HomeController', ['$scope', function($scope) {
 cvAppControllers.controller('AboutController', ['$scope', function($scope) {
 	$scope.name = 'Elias';
 }]);
-
-cvAppControllers.controller('HomeController', ['$scope', '$http', function($scope, $http) {
-	$http.get('info/info.json').success(function(data) {
-		$scope.info = data;
-	});
-}]);
-
-
-cvAppControllers.controller('AboutController', ['$scope', '$http', function($scope, $http) {
-	$http.get('info/info.json').success(function(data) {
-		$scope.info = data;
-	});
-}]);
