1c6fc36a1fe93251f6771512aedef75e7c031cb9
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-10 12:37:10 +0200

    Add routes to cvApp module

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,18 @@
 'use strict';
 
 var cvApp = angular.module('cvApp', ['ngRoute', 'cvAppControllers']);
+
+cvApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+	.when('/home', {
+		templateUrl: 'views/home.html',
+		controller: 'HomeController'
+	})
+	.when('/about', {
+		templateUrl: 'views/about.html',
+		controller: 'AboutController'
+	})
+	.otherwise({
+		redirectTo: '/home'
+	});
+}]);
