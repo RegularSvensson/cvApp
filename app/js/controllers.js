9f7c909ceebe0a587a45fe273ac3fe794bb598a8
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-10 17:19:56 +0200

    Add console.log for controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,11 +8,14 @@ cvAppControllers.controller('HomeController', ['$scope', '$http', function($scop
 	$http.get('/app/info/info.json').success(function(data) {
 		$scope.stuff = data;
 	});
-	$scope.backgroundImg = "/app/img/kugghjulet.png"
+	$scope.backgroundImg = "/app/img/kugghjulet.png";
+	console.log("I'm in home! :D")
 }]);
 
 cvAppControllers.controller('AboutController', ['$scope', '$http', function($scope, $http) {
 	$http.get('/app/info/info.json').success(function(data) {
 		$scope.stuff = data;
+
 	});
+	console.log("I'm in about! :D")
 }]);
