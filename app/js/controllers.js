34f00bae571e14fed70294b31eb534f91262a661
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-10 12:44:46 +0200

    Add boilerplate for controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* Controllers */
+
 var cvAppControllers = angular.module('cvAppControllers', []);
 
 cvAppControllers.controller('HomeController', ['$scope', '$http', function($scope, $http) {
