c7a2fd926d6656d3a6d23c8571b2d7d071251ad8
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-10 14:45:22 +0200

    Add functioning controller that loads json

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,20 +8,8 @@ cvAppControllers.controller('HomeController', ['$scope', function($scope) {
 	$scope.name = 'Elias';
 }]);
 
-
-
-/*
-cvAppControllers.controller('AboutController', ['$scope', function($scope) {
-	$scope.name = 'Elias';
+cvAppControllers.controller('AboutController', ['$scope', '$http', function($scope, $http) {
+	$http.get('/app/info/info.json').success(function(data) {
+		$scope.stuff = data;
+	});
 }]);
-*/
-
-/*
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('phones/phones.json').success(function(data) {
-      $scope.phones = data;
-    });
-    $scope.orderProp = 'age';
-  }]);
-*/
