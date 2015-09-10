754fe14789756a8e1cf57128d7b878c83be326c1
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-10 13:32:35 +0200

    Add working controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,14 @@
 
 var cvAppControllers = angular.module('cvAppControllers', []);
 
+cvAppControllers.controller('HomeController', ['$scope', function($scope) {
+	$scope.name = 'Elias';
+}]);
+
+cvAppControllers.controller('AboutController', ['$scope', function($scope) {
+	$scope.name = 'Elias';
+}]);
+
 cvAppControllers.controller('HomeController', ['$scope', '$http', function($scope, $http) {
 	$http.get('info/info.json').success(function(data) {
 		$scope.info = data;
