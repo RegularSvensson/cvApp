42153f81b1d1b8086b804af486fc457a550c249a
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-11 21:54:55 +0200

    simplify folder urls

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,7 @@
 var cvAppControllers = angular.module('cvAppControllers', []);
 
 cvAppControllers.controller('HomeController', ['$scope', '$http', function($scope, $http) {
-	$http.get('/app/info/info.json').success(function(data) {
+	$http.get('info/info.json').success(function(data) {
 		$scope.stuff = data;
 	});
 	$scope.backgroundImg = "/app/img/kugghjulet.png";
@@ -13,7 +13,7 @@ cvAppControllers.controller('HomeController', ['$scope', '$http', function($scop
 }]);
 
 cvAppControllers.controller('AboutController', ['$scope', '$http', function($scope, $http) {
-	$http.get('/app/info/info.json').success(function(data) {
+	$http.get('info/info.json').success(function(data) {
 		$scope.stuff = data;
 
 	});
