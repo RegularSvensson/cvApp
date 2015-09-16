a1568333b7f0db465d19eaf5cb68f6b832a0d9fb
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-16 13:37:17 +0200

    Add rename to compressJS

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,8 @@ gulp.task('default', function() {
 gulp.task('compressJS', function() {
   return gulp.src('/app/js/*.js')
     .pipe(uglify())
+    .pipe(rename({
+    	suffix: '.min'
+    }))
     .pipe(gulp.dest('/app/js/'));
 });
