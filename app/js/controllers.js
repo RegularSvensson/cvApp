78f755209c38b68737b79b324d759b22b0301fb2
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-16 14:37:07 +0200

    Change reference to info.min.json

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,14 +5,14 @@
 var cvAppControllers = angular.module('cvAppControllers', []);
 
 cvAppControllers.controller('HomeController', ['$scope', '$http', function($scope, $http) {
-	$http.get('/app/info/info.json').success(function(data) {
+	$http.get('/app/info/info.min.json').success(function(data) {
 		$scope.info = data;
 	});
 	console.log("I'm in home! :D")
 }]);
 
 cvAppControllers.controller('AboutController', ['$scope', '$http', function($scope, $http) {
-	$http.get('/app/info/info.json').success(function(data) {
+	$http.get('/app/info/info.min.json').success(function(data) {
 		$scope.info = data;
 	});
 	console.log("I'm in about! :D")
